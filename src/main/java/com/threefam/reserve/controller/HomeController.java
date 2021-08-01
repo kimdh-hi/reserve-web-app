e2f0476524e86282fd6d3394435fe0d09e3a71bb
kimdaehyun <zbeld123@gmail.com>
2021-08-01 22:41:52 +0900

    home controller test method

--- src/main/java/com/threefam/reserve/controller/HomeController.java
+++ src/main/java/com/threefam/reserve/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.threefam.reserve.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String home() {
+        return "hello";
+    }
 }
