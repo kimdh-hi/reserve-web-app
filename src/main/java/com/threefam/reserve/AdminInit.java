2a36a1c0f3b92eccbf6e1413bf6c771ce7ff80f8
sungjin <sungjin0757@naver.com>
2021-08-05 17:06:00 +0900

    add PostConstruct

---
+++ src/main/java/com/threefam/reserve/AdminInit.java
@@ -0,0 +1,38 @@
+package com.threefam.reserve;
+
+import com.threefam.reserve.domain.entity.User;
+import com.threefam.reserve.domain.value.Gender;
+import com.threefam.reserve.domain.value.Role;
+import com.threefam.reserve.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 초기 admin 데이터 저장 클래스, DI(의존 관계) 주입 후 바로 실행 될 메서드 정의
+ */
+@Component
+@RequiredArgsConstructor
+public class AdminInit {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostConstruct
+    public void init(){
+        User admin = User.createUser()
+                .email("admin")
+                .password(bCryptPasswordEncoder.encode("admin"))
+                .name("admin")
+                .age(0)
+                .address("admin")
+                .detailAddress("admin")
+                .role(Role.ROLE_ADMIN)
+                .gender(Gender.MALE)
+                .build();
+
+        userService.createUser(admin);
+    }
+}
---
+++ src/test/java/com/threefam/reserve/AdminInitTest.java
@@ -0,0 +1,31 @@
+package com.threefam.reserve;
+
+import com.threefam.reserve.domain.entity.User;
+import com.threefam.reserve.domain.value.Role;
+import com.threefam.reserve.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * web application 실행 시, admin이 등록되어 있는지 테스의
+ */
+
+@SpringBootTest
+@Transactional
+public class AdminInitTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void admin_등록(){
+        User admin = userService.getUserByEmail("admin");
+
+        Assertions.assertThat(admin.getEmail()).isEqualTo("admin");
+        Assertions.assertThat(admin.getRole()).isEqualTo(Role.ROLE_ADMIN);
+    }
+}
