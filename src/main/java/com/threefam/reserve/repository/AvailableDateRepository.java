ce65de8977b142c8589e77b207aa39f2613912b4
kimdaehyun <zbeld123@gmail.com>
2021-08-12 13:16:15 +0900

    예약하기 - 예약데이터 받아오기

---
+++ src/main/java/com/threefam/reserve/repository/AvailableDateRepository.java
@@ -0,0 +1,7 @@
+package com.threefam.reserve.repository;
+
+import com.threefam.reserve.domain.entity.AvailableDate;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AvailableDateRepository extends CrudRepository<AvailableDate, Long> {
+}
---
+++ src/main/java/com/threefam/reserve/repository/custom/ReserveItemCustomRepository.java
@@ -0,0 +1,58 @@
+package com.threefam.reserve.repository.custom;
+
+import com.threefam.reserve.domain.entity.AvailableDate;
+import com.threefam.reserve.domain.entity.AvailableTime;
+import com.threefam.reserve.domain.entity.Vaccine;
+import com.threefam.reserve.repository.HospitalRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ReserveItemCustomRepository {
+
+    private final EntityManager em;
+    private final HospitalRepository hospitalRepository;
+
+    /**
+     * 예약가능 날짜 조회
+     */
+    public List<AvailableDate> findAvailableDatesByHospitalId(Long id) {
+        return em.createQuery(
+                "select d " +
+                        "from AvailableDate d " +
+                        "where d.hospital.id = :id and d.enabled = true", AvailableDate.class
+        )
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    /**
+     * 예약가능 시간 조회
+     */
+    public List<AvailableTime> findAvailableTimesByAvailableDateId(Long id) {
+        return em.createQuery(
+                "select t " +
+                        "from AvailableTime  t " +
+                        "where t.availableDate.id = :id and t.enabled = true" , AvailableTime.class
+        )
+                .setParameter("id", id)
+                .getResultList();
+    }
+
+    /**
+     * 예약가능 백신이름 조회
+     */
+    public List<Vaccine> findAvailableVaccines(String hospitalName) {
+        return em.createQuery(
+                "select v " +
+                        "from Vaccine v " +
+                        "where v.hospital.hospitalName = :hospitalName and v.quantity > 0", Vaccine.class
+        )
+                .setParameter("hospitalName", hospitalName)
+                .getResultList();
+    }
+}
